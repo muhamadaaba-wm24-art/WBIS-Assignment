5e9f45e894363fdd511aef3798473e58fc8940b2
muhamadaaba-wm24-art <muhamadaaba-wm24@student.tarc.edu.my>
2025-08-27 15:23:30 +0800

    loyalty table for loyalty php

--- sql/web_db.sql
+++ sql/web_db.sql
@@ -277,6 +277,18 @@ CREATE TABLE `wishlist` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
+-- Table structure for table `loyalty`
+--
+
+DROP TABLE IF EXISTS `loyalty`;
+CREATE TABLE `loyalty` (
+  `loyalty_id` varchar(11) NOT NULL,
+  `user_id` varchar(11) NOT NULL,
+  `loyalty_point` int(11) NOT NULL,
+  `discount_rate` int(2) NOT NULL,
+  `loyalty_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+--
 -- Indexes for dumped tables
 --
 
@@ -356,6 +368,14 @@ ALTER TABLE `wishlist`
   ADD KEY `idx_user_id` (`user_id`);
 
 --
+-- Indexes for table `loyalty`
+--
+
+ALTER TABLE `loyalty`
+  ADD PRIMARY KEY (`loyalty_id`),
+  ADD KEY `idx_user_id` (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -412,6 +432,13 @@ ALTER TABLE `user_profiles`
 ALTER TABLE `wishlist`
   ADD CONSTRAINT `wishlist_fk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `wishlist_fk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `loyalty`
+--
+
+ALTER TABLE `loyalty`
+  ADD CONSTRAINT `loyalty_fk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
